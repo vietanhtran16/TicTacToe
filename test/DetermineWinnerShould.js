6e1bd1590d9e76d41a4b46f1d6ae216bdea6829b
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-05-21 21:13:17 +1200

    Reorder array so that it looks like a tic tac toe board

--- test/DetermineWinnerShould.js
+++ test/DetermineWinnerShould.js
@@ -5,36 +5,55 @@ var assert = require('assert');
 
 describe('Add player input to array', function () {
     it("Add player input based on specified index - index 3", function () {
-        let expected = {squares: [null, null, null, "X", null, null, null, null, null,], xIsNext: false};
+        let expected = {squares: [null, null, null,
+                                  "X", null, null,
+                                  null, null, null,],
+                                  xIsNext: false};
         let squares = Array(9).fill(null);
         let actual = addPlayerInputToSpecifiedIndex({squares: squares, xIsNext: true}, 3);
         assert.deepEqual(expected, actual);
     });
 
     it("Add player input based on specified index - index 6", function () {
-        let expected = {squares: [null, null, null, null, null, null, "X", null, null,], xIsNext: false};
+        let expected = {squares: [null, null, null,
+                                  null, null, null,
+                                  "X", null, null,],
+                                  xIsNext: false};
         let squares = Array(9).fill(null);
         let actual = addPlayerInputToSpecifiedIndex({squares: squares, xIsNext: true}, 6);
         assert.deepEqual(expected, actual);
     });
 
     it("Update xIsNext property after adding player input", function () {
-        let expected = {squares: [null, null, "X", null, null, null, null, null, null,], xIsNext: false};
+        let expected = {squares: [null, null, "X",
+                                  null, null, null,
+                                  null, null, null,],
+                                  xIsNext: false};
         let squares = Array(9).fill(null);
         let actual = addPlayerInputToSpecifiedIndex({squares: squares, xIsNext: true}, 2);
         assert.deepEqual(expected, actual);
     });
 
     it("Add player input and update xIsNext property when xIsNext is false", function () {
-        let expected = {squares: [null, null, "X", "O", null, "X", "O", null, null,], xIsNext: true};
-        let squares = [null, null, "X", "O", null, "X", null, null, null,];
+        let expected = {squares: [null, null, "X",
+                                  "O", null, "X",
+                                  "O", null, null,],
+                                  xIsNext: true};
+        let squares = [null, null, "X",
+                       "O", null, "X",
+                       null, null, null,];
         let actual = addPlayerInputToSpecifiedIndex({squares: squares, xIsNext: false}, 6);
         assert.deepEqual(expected, actual);
     });
 
     it("Add player input and update xIsNext property when xIsNext is true", function () {
-        let expected = {squares: [null, "X", null, "X", null, "O", "X", "O", null], xIsNext: false};
-        let squares = [null, null, null, "X", null, "O", "X", "O", null];
+        let expected = {squares: [null, "X", null,
+                                  "X", null, "O",
+                                  "X", "O", null],
+                                  xIsNext: false};
+        let squares = [null, null, null,
+                       "X", null, "O",
+                       "X", "O", null];
         let actual = addPlayerInputToSpecifiedIndex({squares: squares, xIsNext: true}, 1);
         assert.deepEqual(expected, actual);
     });
@@ -46,21 +65,27 @@ describe('Add player input to array', function () {
 describe("Get all indexes of most recent player input",function () {
     it("Get all indexes of most recent player input which is O",function () {
         let expect = [1,3];
-        let squares = ["X", "O", null, "O", "X", null, null, null, null];
+        let squares = ["X", "O", null,
+                       "O", "X", null,
+                       null, null, null];
         let actual = GetAllIndexOfRecentInput({squares: squares, xIsNext: true});
         assert.deepEqual(expect, actual);
     });
 
     it("Get all indexes of most recent player input which is X",function () {
         let expect = [0, 2, 5];
-        let squares = ["X", "O", "X", "O", null, "X", null,null,null];
+        let squares = ["X", "O", "X",
+                       "O", null, "X",
+                       null,null,null];
         let actual = GetAllIndexOfRecentInput({squares: squares, xIsNext: false});
         assert.deepEqual(expect, actual);
     });
 
     it("Get all indexes of most recent player input which is O",function () {
         let expect = [1, 3, 7];
-        let squares = ["X", "O", "X", "O", "X", null, null,"O", null];
+        let squares = ["X", "O", "X",
+                       "O", "X", null,
+                       null,"O", null];
         let actual = GetAllIndexOfRecentInput({squares: squares, xIsNext: true});
         assert.deepEqual(expect, actual);
     })
