732a33028c7448f449549c05d144c070dd3691e3
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-05-27 15:06:48 +1200

    Add all code to play console game to PlayTicTacToeConsole function - so that can use in unit test later on

--- ConsoleGame/ConsoleGame.js
+++ ConsoleGame/ConsoleGame.js
@@ -5,25 +5,25 @@ import {DetermineWinner, addPlayerInputToSpecifiedIndex, ConvertCoordinateToArra
 
 var board = {squares: Array(9).fill("  "), xIsNext: true};
 
-while(DetermineWinner(board) == 0 && board.squares.includes("  "))
-{
-    let currentUser = board.xIsNext ? "X" : "O";
-    let squares = board.squares;
-    var userInput = prompt(`Hey ${currentUser}, it is your turn. 
+function PlayTicTacToeConsole(board) {
+    while (DetermineWinner(board) == 0 && board.squares.includes("  ")) {
+        let currentUser = board.xIsNext ? "X" : "O";
+        let squares = board.squares;
+        var userInput = prompt(`Hey ${currentUser}, it is your turn. 
     Play the game by entering row-column coordinate of your squares. For example, 1,1 for top left squares
     Here is what the board looks like
       |1|2|3
     1|${board.squares[0]}|${board.squares[1]}|${board.squares[2]}|
     2|${board.squares[3]}|${board.squares[4]}|${board.squares[5]}|
     3|${board.squares[6]}|${board.squares[7]}|${board.squares[8]}|`);
-    if(userInput == "stop")
-    {
-        break;
+        if (userInput == "stop") {
+            break;
+        }
+        let selectedIndex = ConvertCoordinateToArrayIndex(userInput);
+        addPlayerInputToSpecifiedIndex(board, selectedIndex);
     }
-    let selectedIndex = ConvertCoordinateToArrayIndex(userInput);
-    addPlayerInputToSpecifiedIndex(board, selectedIndex);
-}
-if(DetermineWinner(board)) {
-    let winner = board.xIsNext ? "O" : "X";
-    alert("The winner is " + winner);
+    if (DetermineWinner(board)) {
+        let winner = board.xIsNext ? "O" : "X";
+        alert("The winner is " + winner);
+    }
 }
