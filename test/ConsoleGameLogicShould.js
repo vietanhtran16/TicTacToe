74e9e90d3175cb5194b946be13534b9fee8ff5c2
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-06-15 22:22:06 +1200

    Initial unit test for returnBoardForPrompt

--- test/ConsoleGameLogicShould.js
+++ test/ConsoleGameLogicShould.js
@@ -1,3 +1,26 @@
 /**
  * Created by Viet Anh Tran on 15-Jun-17.
  */
+var fs = require('fs');
+var vm = require('vm');
+var path = 'GameLogic/ConsoleGameLogic.js';
+
+var code = fs.readFileSync(path);
+vm.runInThisContext(code);
+
+var assert = require('assert');
+
+describe("Return current board to display on prompt", function () {
+    it("Display empty board", function () {
+        let expected = "Hey X, it is your turn. " +
+            "Play the game by entering row-column coordinate of your squares. For example, 1,1 for top left squares" +
+            "Here is what the board looks like " +
+            "|1|2|3" +
+            "1|  |  |  |" +
+            "2|  |  |  |" +
+            "3|  |  |  |";
+        var board = {squares: Array(9).fill("  "), xIsNext: true};
+        let actual = returnBoardForPrompt(board);
+        assert.equal(expected, actual);
+    });
+});
