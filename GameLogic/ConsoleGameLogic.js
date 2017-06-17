e53498309519c60d22b273fcd5b4e2404184af4f
vietanhtran1996 <tranvietanh1996@gmail.com>
2017-06-17 12:53:56 +1200

    Add break line to implementation to returnBoardForPrompt

--- GameLogic/ConsoleGameLogic.js
+++ GameLogic/ConsoleGameLogic.js
@@ -5,12 +5,12 @@ function returnBoardForPrompt(board) {
     let currentUser = board.xIsNext ? "X" : "O";
     let squares = board.squares;
     var promptMessage = `Hey ${currentUser}, it is your turn. ` +
-        "Play the game by entering row-column coordinate of your squares. For example, 1,1 for top left squares" +
-        "Here is what the board looks like " +
-        "|1|2|3" +
-        `1|${board.squares[0]}|${board.squares[1]}|${board.squares[2]}|` +
-        `2|${board.squares[3]}|${board.squares[4]}|${board.squares[5]}|` +
-        `3|${board.squares[6]}|${board.squares[7]}|${board.squares[8]}|`;
+        "\nPlay the game by entering row-column coordinate of your squares. For example, 1,1 for top left squares" +
+        "\nHere is what the board looks like " +
+        "\n|1|2|3" +
+        `\n1|${board.squares[0]}|${board.squares[1]}|${board.squares[2]}|` +
+        `\n2|${board.squares[3]}|${board.squares[4]}|${board.squares[5]}|` +
+        `\n3|${board.squares[6]}|${board.squares[7]}|${board.squares[8]}|`;
     return promptMessage;
 }
 
